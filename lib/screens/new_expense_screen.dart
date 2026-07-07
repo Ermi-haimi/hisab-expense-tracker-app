2917a9ed712d412af3b444f304a0ae776f20b674
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-07-07 13:42:39 +0300

    styled new expense screen

--- lib/screens/new_expense_screen.dart
+++ lib/screens/new_expense_screen.dart
@@ -28,7 +28,7 @@ class _NewExpenseScreenState extends State<NewExpenseScreen> {
   final _nameController = TextEditingController();
   final _noteController = TextEditingController();
   DateTime selectedDate = DateTime.now();
-  String selectedCategory = "Food";
+  String selectedCategory = 'Food';
 
   Future<void> _pickDate() async {
     final pickedDate = await showDatePicker(
@@ -74,6 +74,7 @@ class _NewExpenseScreenState extends State<NewExpenseScreen> {
               Container(
                 width: double.infinity,
                 padding: EdgeInsets.only(top: 10, left: 10),
+                margin: EdgeInsets.only(bottom: 15),
                 child: Text(
                   'Add Expenses',
                   style: TextStyle(
@@ -83,104 +84,286 @@ class _NewExpenseScreenState extends State<NewExpenseScreen> {
                   textAlign: TextAlign.start,
                 ),
               ),
-              TextFormField(
-                controller: _amountController,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '12.50',
-                  labelText: 'Amount',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Enter an amount";
-                  }
-
-                  final amount = double.tryParse(value);
-
-                  if (amount == null || amount <= 0) {
-                    return "Enter a valid amount";
-                  }
-
-                  return null;
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Donut',
-                  labelText: 'Name',
-                ),
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "Please enter a name";
-                  }
-
-                  return null;
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                controller: _noteController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Note (Optional)",
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text(
-                    "Date",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Color.fromRGBO(158, 158, 158, 0.5),
+                    width: 1,
                   ),
-                  const SizedBox(height: 8),
-                  OutlinedButton.icon(
-                    onPressed: _pickDate,
-                    icon: const Icon(Icons.calendar_today),
-                    label: Text(
-                      "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}",
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Amount*',
+                      style: TextStyle(
+                        fontWeight: FontWeight(600),
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              GridView.count(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                crossAxisCount: 2,
-                mainAxisSpacing: 12,
-                crossAxisSpacing: 10,
-                childAspectRatio: 3,
-                children: List.generate(
-                  categories.length,
-                  (index) {
-                    final category = categories[index];
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      controller: _amountController,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusColor: Color(0xFF4F39F6),
+                        hintText: '12.50',
+                        prefixIcon: Icon(Icons.attach_money),
+                      ),
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight(600),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Enter an amount";
+                        }
+                        final amount = double.tryParse(value);
+                        if (amount == null || amount <= 0) {
+                          return "Enter a valid amount";
+                        }
 
-                    return ChoiceChip(
-                      showCheckmark: false,
-                      label: Center(child: Text(category)),
-                      selected: selectedCategory == category,
-                      onSelected: (selected) {
-                        setState(() {
-                          selectedCategory = category;
-                        });
+                        return null;
                       },
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-              ElevatedButton(
-                onPressed: _saveExpense,
-                child: const Text("Save"),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Color.fromRGBO(158, 158, 158, 0.5),
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Description*',
+                      style: TextStyle(
+                        fontWeight: FontWeight(600),
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusColor: Color(0xFF4F39F6),
+                        hintText: 'Donut',
+                        prefixIcon: Icon(Icons.legend_toggle),
+                      ),
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight(600),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Please Enter a Description or Name";
+                        }
+
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Color.fromRGBO(158, 158, 158, 0.5),
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Category*',
+                      style: TextStyle(
+                        fontWeight: FontWeight(600),
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GridView.count(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 12,
+                      crossAxisSpacing: 10,
+                      childAspectRatio: 3,
+                      children: List.generate(
+                        categories.length,
+                        (index) {
+                          final category = categories[index];
+
+                          return ChoiceChip(
+                            padding: EdgeInsets.all(0),
+                            labelPadding: EdgeInsets.all(0),
+                            showCheckmark: false,
+                            label: Center(
+                              child: Text(
+                                category,
+                              ),
+                            ),
+
+                            selected: selectedCategory == category,
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedCategory = category;
+                              });
+                            },
+                            selectedColor: Color.fromRGBO(239, 246, 255, 0.5),
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              side: BorderSide(
+                                color: selectedCategory == category
+                                    ? Color(0xFF4F39F6)
+                                    : Color.fromRGBO(178, 178, 178, 0.2),
+                                width: 2,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Color.fromRGBO(158, 158, 158, 0.5),
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight(600),
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    OutlinedButton.icon(
+                      onPressed: _pickDate,
+                      icon: Icon(Icons.calendar_today),
+                      label: Text(
+                        "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Color.fromRGBO(158, 158, 158, 0.5),
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'DateNote (Optional)',
+                      style: TextStyle(
+                        fontWeight: FontWeight(600),
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      controller: _noteController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusColor: Color(0xFF4F39F6),
+                        hintText: '...',
+                        prefixIcon: Icon(Icons.legend_toggle),
+                      ),
+                      minLines: 2,
+                      maxLines: 3,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: _saveExpense,
+
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF4F39F6),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Text(
+                    "Add Expense",
+                    style: TextStyle(
+                      fontWeight: FontWeight(600),
+                      fontSize: 23,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
