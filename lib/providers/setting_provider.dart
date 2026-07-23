d3c0206e953b287c5512ef4eca51ecb9dab68fa3
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-07-24 00:39:04 +0300

    made the theme choice persistent

--- lib/providers/setting_provider.dart
+++ lib/providers/setting_provider.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingProvider extends ChangeNotifier {
+  SettingProvider() {
+    loadTheme();
+  }
+
   final Box settingBox = Hive.box('setting');
   ThemeMode _themeMode = ThemeMode.light;
 
@@ -22,10 +27,25 @@ class SettingProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void toggleTheme() {
+  Future<void> toggleTheme() async {
     _themeMode = _themeMode == ThemeMode.dark
         ? ThemeMode.light
         : ThemeMode.dark;
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setBool(
+      'isDarkMode',
+      _themeMode == ThemeMode.dark,
+    );
+    notifyListeners();
+  }
+
+  Future<void> loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    bool isDark = prefs.getBool('isDarkMode') ?? false;
+
+    _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
 
     notifyListeners();
   }
